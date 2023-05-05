3dbb2395c6507eb4ce43ce402168a25e62cb217c
diar <umarovdiar4@gmail.com>
2023-05-05 23:26:28 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,128 @@
+
+ "use strict";
+ 
+// let number = 5.7; //Data type = number 
+
+// console.log(-8 / 0); //-infinity cheksiz
+// console.log("diar" * 8); //Nan matematikada yoq narsa
+
+//                    //primative
+//  //string
+// const clientname = "Diar";
+
+
+// //boolean
+// const isMarried = true ;
+
+
+// //null
+// console.log(clientAge); //Null 
+
+
+// //underfined
+// let und;
+// console.log(und);
+       
+
+
+
+//             //object
+// const thief =  {
+//     age:30, //number
+//     JAcket:"balck",
+//     long:false, //boolean
+// }
+//        console.log(thief.JAcket);       
+   
+
+
+//        //array\
+
+//        const colors = ['black', 'red' ,'green'];
+//        console.log(colors [1]);
+
+
+
+
+
+
+
+
+
+
+
+//  const place = confirm("Are you hare");
+//  console.log(place);
+
+
+
+// const currentPlace = prompt("where are you","samsar")
+
+// console.log(currentPlace);
+
+ 
+// const age = +prompt("how old are you", "18")
+// console.log(age + 10);
+
+// const favoriteColor = [];
+// favoriteColor[0] = prompt("whats you favorite color #1","" );
+// favoriteColor[1] = prompt("whats you favorite color #2","" );
+// favoriteColor[2] = prompt("whats you favorite color #3","" );
+
+// console.log(favoriteColor);
+
+
+ 
+//   const channel = prompt("what's you favorite youtuber")
+//    console.log(` https://youtube.com/ ${channel}` );
+
+
+
+
+// const user = "samar";
+//  const job = "javaScript developer"
+
+//  console.log(`user name is ${user},he is ${job}`);
+
+
+ 
+
+       
+ 
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
